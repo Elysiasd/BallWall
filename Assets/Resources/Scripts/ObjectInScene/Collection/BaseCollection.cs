a296bc119b970b3bf0fb1049e296fb006bf60844
FugerQingliu <1422315218@qq.com>
2024-05-13 13:42:30 +0800

    Change SlideWall Direction

--- Assets/Resources/Scripts/ObjectInScene/Collection/BaseCollection.cs
+++ Assets/Resources/Scripts/ObjectInScene/Collection/BaseCollection.cs
@@ -1,43 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Rendering;
 
 /// <summary>
 /// 基本的收集品的属性和函数
 /// </summary>
 public abstract class BaseCollection : MonoBehaviour
 {
-    [SerializeField] protected AudioClip clip;
-    [SerializeField] private float collectTime = 1f;
-    protected abstract string colName { get; }
+    [SerializeField]protected AudioClip clip;
+    protected abstract  string colName { get; }
     private CollectionManager collectionManager;
-
-    private Vector3 origin;
 
     protected virtual void Start()
     {
         collectionManager = CollectionManager.Instance;
         collectionManager.StartCnt(colName);
-
-        origin = transform.position;
     }
     protected virtual void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Ball")) { StartCoroutine(Collect()); }
-    }
-    private IEnumerator Collect()
-    {
-        float timer = 0;
-        while (timer < collectTime)
+        if (other.CompareTag("Ball"))
         {
-            transform.position = Vector3.Lerp(origin,
-                Ball.Instance.transform.position, timer / collectTime);
-            timer += Time.deltaTime;
-            yield return null;
+            collectionManager.Collect(colName);
+            AudioManager.Instance.PlayOneShot(clip);
+            Destroy(gameObject);
+            //TODO
+
         }
-        collectionManager.Collect(colName);
-        AudioManager.Instance.PlayOneShot(clip);
-        Destroy(gameObject);
     }
+
+
 }
